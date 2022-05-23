4e904b85229fa7de46addbf119bb028daf0ebf72
Pat B <patrick.bornay@gmail.com>
2022-05-23 13:22:05 +1000

    some closure exercise solutions

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const onceFunc = once(addByTwo);
+// console.log(onceFunc(4));  // => should log 6
+// console.log(onceFunc(10));  // => should log 6
+// console.log(onceFunc(9001));  // => should log 6
